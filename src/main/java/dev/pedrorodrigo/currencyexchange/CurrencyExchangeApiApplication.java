31ba271946760ca6609c4799855c30c51779697d
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-18 15:03:38 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/dev/pedrorodrigo/currencyexchange/CurrencyExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package dev.pedrorodrigo.currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeApiApplication.class, args);
+	}
+
+}
